22190af7df259c9cd7b9aad4087b4d3b0474577e
Ton Nom <ton-email@example.com>
2026-06-12 06:13:03 +0200

    Fix robots config

--- app/robots.ts
+++ app/robots.ts
@@ -1,22 +1,22 @@
 import type { MetadataRoute } from "next";
 
-const siteUrl = "https://www.thewinewatchers.com";
-
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: [
-        "/admin",
-        "/api",
-        "/checkout",
-        "/panier",
-        "/connexion",
-        "/inscription",
-        "/mon-compte",
-      ],
-    },
-    sitemap: `${siteUrl}/sitemap.xml`,
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/admin/",
+          "/api/",
+          "/checkout/",
+          "/panier/",
+          "/connexion/",
+          "/inscription/",
+          "/mon-compte/",
+        ],
+      },
+    ],
+    sitemap: "https://www.thewinewatchers.com/sitemap.xml",
   };
 }
