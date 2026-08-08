9b0f5907b963b02d3aa4ec681e9f0e45b56f8e4d
Ton Nom <ton-email@example.com>
2026-08-08 09:26:46 +0200

    Corrige les redirections Valmur et Cheval Blanc

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,18 @@ const nextConfig: NextConfig = {
         destination: "/boutique/primeurs-2025",
         permanent: true,
       },
+      {
+        source: "/boutique/vin/chablis-grand-cru-valmur-2023",
+        destination:
+          "/boutique/vin/chablis-grand-cru-valmur-2023-raveneau",
+        permanent: true,
+      },
+      {
+        source:
+          "/stemilion-chateau-cheval-blanc-xsl-253_259_310.html",
+        destination: "/producteur/chateau-cheval-blanc",
+        permanent: true,
+      },
     ];
   },
 };
